592fbc44c2e2b04d0596351d0f39ef6c3b47139a
Henri Mäkelä <henri.makela@boogiesoftware.com>
2020-06-26 15:10:26 +0300

    add required fragments

---
+++ app/src/main/java/fi/henrimakela/backbase_challenge/fragments/MapFragment.kt
@@ -0,0 +1,24 @@
+package fi.henrimakela.backbase_challenge.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import fi.henrimakela.backbase_challenge.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MapFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+}
