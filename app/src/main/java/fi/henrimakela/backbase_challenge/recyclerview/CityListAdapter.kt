680e42ecaf4a92ba806defc47bfdf9e14bb829fa
Henri Mäkelä <henri.makela@boogiesoftware.com>
2020-06-29 14:22:27 +0300

    move the filter code to an utility class

--- app/src/main/java/fi/henrimakela/backbase_challenge/recyclerview/CityListAdapter.kt
+++ app/src/main/java/fi/henrimakela/backbase_challenge/recyclerview/CityListAdapter.kt
@@ -1,4 +1,5 @@
 package fi.henrimakela.backbase_challenge.recyclerview
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
@@ -6,17 +7,20 @@ import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import fi.henrimakela.backbase_challenge.R
 import fi.henrimakela.backbase_challenge.data_classes.City
+import fi.henrimakela.backbase_challenge.util.ListFilterer
 import java.util.*
 import kotlin.collections.ArrayList
 
 
-class CityListAdapter(var cityList: ArrayList<City>, val listener: OnCitySelectedListener) : RecyclerView.Adapter<CityViewHolder>(), Filterable {
+class CityListAdapter(var cityList: ArrayList<City>, val listener: OnCitySelectedListener) :
+    RecyclerView.Adapter<CityViewHolder>(), Filterable {
 
     private var cityListFull: ArrayList<City> = ArrayList(cityList)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.city_list_item, parent, false)
-        return CityViewHolder(view )
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.city_list_item, parent, false)
+        return CityViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -34,7 +38,7 @@ class CityListAdapter(var cityList: ArrayList<City>, val listener: OnCitySelecte
 
     }
 
-    fun setCities(cities: ArrayList<City>){
+    fun setCities(cities: ArrayList<City>) {
         this.cityList = cities
         this.cityListFull = ArrayList(cities)
 
@@ -45,61 +49,12 @@ class CityListAdapter(var cityList: ArrayList<City>, val listener: OnCitySelecte
         return filter
     }
 
-    private val filter :Filter = object : Filter(){
+    private val filter: Filter = object : Filter() {
         override fun performFiltering(constraint: CharSequence?): FilterResults {
-            val filteredList: ArrayList<City> = ArrayList()
 
-            if(constraint == null || constraint.isEmpty()){
-                filteredList.addAll(cityListFull)
-            }
-            else {
-
-
-
-                //linear approach
-                /*var searchWord = constraint.toString()
-                cityListFull.forEach {
-                    if(it.name.startsWith(searchWord, true)){
-                        filteredList.add(it)
-                    }
-                }
-
-            */
-
-                //non-linear approach
-                var searchWord = constraint.toString().trim()
-                var index = binarySearch(cityListFull, searchWord)
-
-                if (index > -1){
-                    var bIndex = index;
-                    var fIndex = index;
-
-                    //go backwards on the list
-                    while(bIndex > 0){
-                        if(cityListFull[bIndex].name.startsWith(searchWord, true)){
-                            filteredList.add(cityListFull[bIndex])
-                            bIndex--
-                        }
-                        else{
-                            break
-                        }
-                    }
-                    // go forward on the list
-                    while(fIndex < cityListFull.size){
-                        if(cityListFull[fIndex].name.startsWith(searchWord, true)){
-                            filteredList.add(cityListFull[fIndex])
-                            fIndex++
-                        }
-                        else{
-                            break
-                        }
-                    }
-
-                }
-            }
 
             val results = FilterResults()
-            results.values = filteredList
+            results.values = ListFilterer.filterResults(cityListFull, constraint.toString().trim())
 
             return results
         }
@@ -111,28 +66,6 @@ class CityListAdapter(var cityList: ArrayList<City>, val listener: OnCitySelecte
         }
 
     }
-
-    private fun binarySearch(list: List<City>, searchWord: String): Int{
-        var low = 0
-        var high = list.size - 1
-
-        while(low <= high){
-            var mid = (low + high) / 2
-            if(list.get(mid).name.startsWith(searchWord, true)){
-                return mid
-            }
-            else if(searchWord.toLowerCase() < list[mid].name.toLowerCase()){
-                high = mid - 1
-            }
-            else{
-                low = mid + 1
-            }
-
-        }
-
-        return -1
-    }
-
 
 }
 
---
+++ app/src/main/java/fi/henrimakela/backbase_challenge/util/ListFilterer.kt
@@ -0,0 +1,82 @@
+package fi.henrimakela.backbase_challenge.util
+
+import fi.henrimakela.backbase_challenge.data_classes.City
+
+class ListFilterer {
+
+    companion object{
+        fun filterResults(list : ArrayList<City>, searchWord: String): ArrayList<City>{
+
+
+
+            if(searchWord.isNullOrEmpty()){
+                return list
+            }
+            var filteredList: ArrayList<City> = arrayListOf()
+
+            /*//linear approach
+            list.forEach {
+                if(it.name.startsWith(searchWord, true)){
+                    filteredList.add(it)
+                }
+            }*/
+
+
+            //non-linear approach
+            var index = binarySearch(list, searchWord)
+
+            if (index > -1){
+                var bIndex = index;
+                var fIndex = index;
+
+                //go backwards on the list
+                while(bIndex > 0){
+                    if(list[bIndex].name.startsWith(searchWord, true)){
+                        filteredList.add(list[bIndex])
+                        bIndex--
+                    }
+                    else{
+                        break
+                    }
+                }
+                // go forward on the list
+                while(fIndex < list.size){
+                    if(list[fIndex].name.startsWith(searchWord, true)){
+                        filteredList.add(list[fIndex])
+                        fIndex++
+                    }
+                    else{
+                        break
+                    }
+                }
+
+            }
+
+            return filteredList
+        }
+
+
+        private fun binarySearch(list: List<City>, searchWord: String): Int{
+            var low = 0
+            var high = list.size - 1
+
+            while(low <= high){
+                var mid = (low + high) / 2
+                if(list.get(mid).name.startsWith(searchWord, true)){
+                    return mid
+                }
+                else if(searchWord.toLowerCase() < list[mid].name.toLowerCase()){
+                    high = mid - 1
+                }
+                else{
+                    low = mid + 1
+                }
+
+            }
+
+            return -1
+        }
+    }
+
+
+}
